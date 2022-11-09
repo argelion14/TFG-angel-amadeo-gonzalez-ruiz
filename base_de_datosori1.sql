67a2674143f390cad9ef9347b07c9b320c977437
argelion14 <argelion14@gmail.com>
2022-11-09 16:10:23 +0100

    Limpieza de código

--- base_de_datosori1.sql
+++ base_de_datosori1.sql
@@ -16,7 +16,7 @@
 
 
 -- Volcando estructura de base de datos para ori
-CREATE DATABASE IF NOT EXISTS `ori` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `ori`;
 USE `ori`;
 
 -- Volcando estructura para tabla ori.periodicidad
